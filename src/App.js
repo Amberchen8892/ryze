75ad3f2e53cb3d96feffc0b6f3a501997583f0c8
Phuong Tran <phuongtran@Phuongs-MBP.localdomain>
2021-10-11 15:30:14 -0600

    remove unused components

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import './App.css';
 import NavBar from './pages/NavBar';
 import Home from './pages/Home';
 import Footer from './pages/Footer';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
 import Questions from './pages/Questions';
 
 function App() {
@@ -16,8 +14,6 @@ function App() {
         <section>
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path="/signup" component={Signup} />
-            <Route path="/login" component={Login} />
             <Route path="/questions" component={Questions} />
           </Switch>
         </section>
--- src/components/FormFields/index.js
+++
@@ -1,7 +0,0 @@
-import InputField from './InputField';
-import CheckboxField from './CheckboxField';
-import SelectField from './SelectField';
-import DatePickerField from './DatePickerField';
-import MyRadioForm from './MyRadioForm';
-
-export { InputField, CheckboxField, SelectField, DatePickerField, MyRadioForm };
